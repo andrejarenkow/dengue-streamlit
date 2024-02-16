fdbb9f26728b0ebe9343a24443e61056b25a1ed2
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-16 16:17:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ import plotly.express as px
 arquivo = 'https://ti.saude.rs.gov.br/ws/dengue_resid_csv.csv'
 dados_dengue = pd.read_csv(arquivo, sep=',', encoding='latin1')
 
+# Criar um input widget (filtro)
+ano = st.selectbox('Selecione o ano', dados_dengue['Ano'].unique())
 
-dados_dengue_2024 = dados_dengue.loc[dados_dengue['Ano']==2024]
+
+# Filtrar o dataframe
+dados_dengue_2024 = dados_dengue.loc[dados_dengue['Ano']==ano]
 dados_dengue_2024
