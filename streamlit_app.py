99bbe7192a6dabb55c7524b63f86128bfec02476
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-16 16:09:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ import plotly.express as px
 # Carrega dados de casos de dengue de um arquivo CSV
 arquivo = 'https://ti.saude.rs.gov.br/ws/dengue_resid_csv.csv'
 dados_dengue = pd.read_csv(arquivo, sep=',', encoding='latin1')
-st.write('Olá, mundo')
+st.write('Olá, mai e gayndalf')
