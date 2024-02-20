4041c3e40d080ca03026103d860a50d92960857c
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-20 10:14:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,31 +17,38 @@ col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png
 col2.header('Painel Alternativo Dengue')
 col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
 
+#Layout padrão
+coluna_filtros, coluna_dados = st.columns([1,4])
+
+
 # Carrega dados de casos de dengue de um arquivo CSV
 arquivo = 'https://ti.saude.rs.gov.br/ws/dengue_resid_csv.csv'
 dados_dengue = pd.read_csv(arquivo, sep=',', encoding='latin1')
 
 # Criar um input widget (filtro)
-ano = st.selectbox('Selecione o ano', sorted(dados_dengue['Ano'].unique()), index=9)
+with coluna_filtros:
+    ano = st.selectbox('Selecione o ano', sorted(dados_dengue['Ano'].unique()), index=9)
 
 # Filtrar o dataframe
 dados_dengue_ano = dados_dengue.loc[dados_dengue['Ano']==ano]
 
 # Qual index
-index_selecionado = st.selectbox('Selecione qual variável quer na linha', ['Nome Município', 'CRS'])
+with coluna_filtros:
+    index_selecionado = st.selectbox('Selecione qual variável quer na linha', ['Nome Município', 'CRS'])
 
 # Create a pivot table
 pivot_table = pd.pivot_table(dados_dengue_ano, values='Confirmados', index=index_selecionado, columns='Semana Epidemiol\u00f3gica', aggfunc='sum', fill_value=0)
 
 # Print the pivot table
-pivot_table
+with coluna_dados:
+    pivot_table
 
 #teste 1
 dados_dengue_final = dados_dengue.set_index("Cód IBGE")
 
 # Plotar um botão
-if st.button('Clique aqui'):
-    st.write('Botão clicado!')
+#if st.button('Clique aqui'):
+#    st.write('Botão clicado!')
   
 #testa mapa
 #map_fig = px.choropleth_mapbox(dados_dengue, geojson=dados_andre.geometry,
