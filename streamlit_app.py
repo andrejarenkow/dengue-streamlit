e12f2d305aafd6bc227ca926a231da8e9ff762bb
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-22 10:35:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ with container_filtros:
 with container_filtros:
     lista_crs = sorted((dados_dengue['CRS'].unique()).tolist())
     lista_crs.append('Todas')
-    crs_selecionada = st.selectbox('Selecione qual variável quer na linha', lista_crs, index=18)
+    crs_selecionada = st.selectbox('Selecione a CRS', lista_crs, index=18)
 
 # Filtrar o dataframe
 if crs_selecionada == 'Todas':
