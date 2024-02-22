58f8b6baf27d9efa7fd3d268c84683c1ad69d8e8
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-22 14:32:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,12 @@ altura_dinamica = 800/24*len(pivot_table)
 with coluna_dados:
     aba_confirmados, aba_notificacoes = st.tabs(['Confirmados', 'Notificações'])
     with aba_confirmados:
-        heatmap_fig_conf = px.imshow(pivot_table, text_auto=True, color_continuous_scale='OrRd', width=800, aspect ='auto',#height=altura_dinamica, 
+        heatmap_fig_conf = px.imshow(pivot_table, text_auto=True, color_continuous_scale='OrRd', width=800, height=altura_dinamica, 
                                 title=f'Casos confirmados por semana epidemiológica por município, RS, {ano}')
         st.plotly_chart(heatmap_fig_conf, use_container_width=False)
 
     with aba_notificacoes:
-        heatmap_fig_notific = px.imshow(pivot_table_notific, text_auto=True, color_continuous_scale='Blues', width=800, height=altura_dinamica, aspect ='equal',
+        heatmap_fig_notific = px.imshow(pivot_table_notific, text_auto=True, color_continuous_scale='Blues', width=800, height=altura_dinamica,
                                 title=f'Notificações por semana epidemiológica por município, RS, {ano}')
         st.plotly_chart(heatmap_fig_notific, use_container_width=False)    
 
