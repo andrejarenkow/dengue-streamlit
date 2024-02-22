b73ea56c233997a526a514e769bcf9dd35a16391
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-02-22 10:18:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ dados_dengue_ano = dados_dengue.loc[dados_dengue['Ano']==ano]
 dados_dengue_ano
 # Qual index
 with container_filtros:
-    crs_selecionada = st.selectbox('Selecione qual variável quer na linha', [1:18])
+    crs_selecionada = st.selectbox('Selecione qual variável quer na linha', range(1,18))
 
 # Create a pivot table
 pivot_table = pd.pivot_table(dados_dengue_ano, values='Confirmados', index=index_selecionado, columns='Semana Epidemiol\u00f3gica', aggfunc='sum', fill_value=0)
