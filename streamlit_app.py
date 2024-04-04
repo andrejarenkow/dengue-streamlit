b9700c763db19cf057fe01b5be86828393b529d1
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-04 13:09:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,7 @@ def ultimas_quatro_semanas():
 
 quatro_ultimas_semanas = ultimas_quatro_semanas()
 filtro_quatro_ultimas_semanas = dados_dengue_ano['Semana Epidemiológica'].isin(quatro_ultimas_semanas)
-st.write(quatro_ultimas_semanas)
+#st.write(quatro_ultimas_semanas)
 
 # Criando DF para usar no mapa
 tabela_mapa = pd.pivot_table(dados_dengue_ano[filtro_quatro_ultimas_semanas], values=['Confirmados', 'Notificações'],
